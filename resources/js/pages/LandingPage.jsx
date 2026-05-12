6f8edcd46e55e2199e6148bfd9d4c17c06651c68
Julia Rebouças <69313019+juliareboucasleite@users.noreply.github.com>
2026-05-12 12:18:22 +0100

    Update LandingPage.jsx

--- resources/js/pages/LandingPage.jsx
+++ resources/js/pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import { useI18n } from '../i18n/I18nContext';
 import { useData } from '../contexts/DataContext';
 import { CarIcon, GaugeIcon, PhoneIcon, PinIcon, SeatIcon } from '../components/Icons';
 import VehicleMedia from '../components/VehicleMedia';
+import { estimateRoute, searchLocations } from '../services/locationService';
 import heroBackground from '../../img/carros/suv-japones-hero.webp';
 
 const LOCATION_OPTIONS = [
